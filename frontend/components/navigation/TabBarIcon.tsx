d8bc8b6199113990f7a401ff78af3135fdaa51eb
mike446 <mikiastadesse83@gmail.com>
2024-10-25 00:38:21 -0400

    testing figma plugin

--- frontend/components/navigation/TabBarIcon.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Ionicons } from '@expo/vector-icons';  // Assuming you're using Expo's icon library
-
-// Define the valid icon names that can be used with Ionicons
-type IoniconNames = keyof typeof Ionicons.glyphMap;
-
-export default function TabBarIcon(props: { name: IoniconNames; color: string }) {
-  return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
-}
